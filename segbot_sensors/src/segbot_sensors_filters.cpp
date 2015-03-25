6cdef101b9f7908ef3302a4e84b90fcda54caa51
Piyush Khandelwal <piyushk@gmail.com>
2015-03-24 22:24:49 -0500

    Revert "Trimmed the branch to only the segbot_firmware sub directory"
    This reverts commit b085dd919906dc92e545728b94e671abfd86dd70.

---
+++ segbot_sensors/src/segbot_sensors_filters.cpp
@@ -0,0 +1,15 @@
+/*
+ * LICENSE: https://github.com/utexas-bwi/segbot/blob/devel/LICENSE
+ */
+
+#include "segbot_sensors/footprint_filter.h"
+#include "segbot_sensors/nan_to_inf_filter.h"
+#include "segbot_sensors/angle_range_filter.h"
+#include "sensor_msgs/LaserScan.h"
+#include "filters/filter_base.h"
+
+#include "pluginlib/class_list_macros.h"
+
+PLUGINLIB_REGISTER_CLASS(segbot_sensors/SegbotFootprintFilter, segbot_sensors::SegbotFootprintFilter, filters::FilterBase<sensor_msgs::LaserScan>)
+PLUGINLIB_REGISTER_CLASS(segbot_sensors/NanToInfFilter, segbot_sensors::NanToInfFilter, filters::FilterBase<sensor_msgs::LaserScan>)
+PLUGINLIB_REGISTER_CLASS(segbot_sensors/AngleRangeFilter, segbot_sensors::AngleRangeFilter, filters::FilterBase<sensor_msgs::LaserScan>)
